26d61f0384300bd0793f9d0be75794488ef9cdf1
baitaowow <2211681859@qq.com>
2026-06-07 07:50:27 +0800

    更新个人主页

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "Baitao",
 
 	// 个人签名
-	bio: "Hi, I'm Ba2itao.",
+	bio: "Hi, I'm Baitao.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
